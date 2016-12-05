680644d25626881731d6787d2b9c02026c26884a
Minh Quan <minh.quan@singalarity.com>
2016-12-06 00:41:49 +0700

    add screen 7 + dialogs >> finish

---
+++ app/src/main/java/se07/smart_ble/ShareActivity.java
@@ -0,0 +1,13 @@
+package se07.smart_ble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
